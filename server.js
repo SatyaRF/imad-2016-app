f9497c8f7e22255ffad53b1d14f9a3d568355d93
Satyadeep Sharma <Satyadeep Sharma>
2016-09-25 02:42:28 +0530

    Changes made locally on 25th Sep, 2016

--- server.js
+++ server.js
@@ -72,6 +72,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var counter = 0;
+app.get('/counter', function(req, res){
+	counter = counter + 1;
+	res.send(counter.toString());
+});
+
 app.get('/:articleName', function(req, res){
 	// articleName == artcile-one
 	// articles[articleName] == {} content object for article one
@@ -100,6 +107,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,53 @@
-console.log('Loaded!');
+// console.log('Loaded!');
+
+// // Change text if div
+
+// // var element = document.getElementById('main-text');
+// // element.innerHTML = 'New Value';	
+
+// //move the image
+// var img = document.getElementById("madi");
+// var marginLeft = 0;
+
+// function moveRight(){
+// 	marginLeft = marginLeft + 10;
+// 	img.style.marginLeft = marginLeft + 'px';
+// }
+
+// img.onclick = function(){
+// 	var interval = setInterval(moveRight, 100); // for every 100 miliseconds moveright function will be called
+// 	//img.style.marginLeft = '100px';
+// };
+
+var button = document.getElementById('counter');
+
+//var counter = 0;
+
+button.onclick = function(){
+
+	//Create a request object
+	var request = new XMLHttpRequest();
+
+	//Capture the response and store it in a variable
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE) {
+			//Take some ction
+			if(request.status == 200){
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	};
+
+	//Make req to counter endpoint
+	request.open('GET', 'http://localhost:8080/counter', true);
+    request.send(null);
+
+
+	// //Render it in correct span
+	// counter = counter + 1;
+	// var span = document.getElementById('count');
+	// span.innerHTML = counter.toString();
+
+}
